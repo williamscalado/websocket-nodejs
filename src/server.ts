cb2c9cafac79218b5677bdbee5281a920383707b
Williams Calado <williams.calado@penseavanti.com.br>
2023-07-17 16:42:49 -0300

    chore(init) start project

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
