0de71231c37132f2ecb2e9e97bb3ce0ef6c5ddf1
Williams Calado <williams.calado@penseavanti.com.br>
2023-07-18 07:49:28 -0300

    chore(client) init client service

--- src/server.ts
+++ src/server.ts
@@ -1,45 +1,32 @@
+import cors from "cors";
 import dotenv from "dotenv";
-import express, { Request, Response } from "express";
-import * as http from "http";
-import * as crypto from "node:crypto";
-import * as WebSocket from "ws";
-
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 dotenv.config();
-
 const app = express();
-const port = process.env.PORT;
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-const clients = new Map();
-wss.on("connection", (ws: WebSocket) => {
-  const id = crypto.randomUUID();
-  const color = Math.floor(Math.random() * 360);
-  const metadata = { id, color };
-  clients.set(ws, metadata);
-  //connection is up, let's add a simple simple event
-  ws.on("message", (message: any) => {
-    const newData = JSON.parse(message);
-    const metadata = clients.get(ws);
+app.use(cors());
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
 
-    newData.sender = metadata.id;
-    newData.color = metadata.color;
-    //log the received message and send it back to the client
-    console.log("received: %s", newData);
-    ws.send(`Hello, you sent -> ${JSON.stringify(newData)}`);
+const dataWord: any = ["luz", "dados"];
+function teste(text: string) {
+  dataWord.push(text);
+  console.log(dataWord);
+}
+
+io.on("connection", (socket) => {
+  socket.on("hello", (arg) => {
+    teste(arg);
+    socket.emit("data", dataWord);
   });
-
-  //send immediatly a feedback to the incoming connection
-  ws.send("Hi there, I am a WebSocket server");
+  socket.emit("data", dataWord);
+  console.log("dados", dataWord);
+  console.log("cliente connect", socket.id);
 });
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
-
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
-
-server.listen(process.env.PORTWS || 8999, () => {
-  console.log(`Server started on port ${server.address() || 8999} :)`);
-});
+httpServer.listen(process.env.PORTWS);
